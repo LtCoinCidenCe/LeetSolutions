2a23acee9761c67d051fe3fc5ded3fcad4cbbc46
Kizuna Akari <lecion20@outlook.com>
2024-09-11 03:38:20 +0800

    offisolu

--- q2555.cpp
+++ q2555.cpp
@@ -4,6 +4,24 @@
 #include <vector>
 using namespace std;
 
+class Solution2 {
+public:
+  int maximizeWin(vector<int> &prizePositions, int k)
+  {
+    int n = prizePositions.size();
+    vector<int> dp(n + 1);
+    int ans = 0;
+    for (int left = 0, right = 0; right < n; right++) {
+      while (prizePositions[right] - prizePositions[left] > k) {
+        left++;
+      }
+      ans = max(ans, right - left + 1 + dp[left]);
+      dp[right + 1] = max(dp[right], right - left + 1);
+    }
+    return ans;
+  }
+};
+
 class Solution {
 public:
   bool doesntOverlap(const pair<int, int> &p1, const pair<int, int> &p2)
