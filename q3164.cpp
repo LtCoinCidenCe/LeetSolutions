14862b049125aca7cdf87e031e50dadd1382c1d3
Kizuna Akari <lecion20@outlook.com>
2024-10-12 00:16:03 +0800

    q3164

---
+++ q3164.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  long long numberOfPairs(vector<int> &nums1, vector<int> &nums2, int k)
+  {
+    unordered_map<int, int> count, count2;
+    int max1 = 0;
+    for (int num : nums1) {
+      count[num]++;
+      max1 = max(max1, num);
+    }
+    for (int num : nums2) {
+      count2[num]++;
+    }
+    long long res = 0;
+    for (const auto &pair : count2) {
+      int a = pair.first, cnt = pair.second;
+      for (int b = a * k; b <= max1; b += a * k) {
+        if (count.count(b) > 0) {
+          res += 1L * count[b] * cnt;
+        }
+      }
+    }
+    return res;
+  }
+};
+
+int main(int argc, char **argv)
+{
+  Solution sln;
+  return 0;
+}
